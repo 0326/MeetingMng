fd074648ba06e227ce8f607c56e97db0faf16e06
wangqiao-hust <wangqiao@hust.edu.cn>
2015-04-02 10:02:48 +0800

    实现了DevAction,DevService,DevDao

--- src/com/huiguanjia/service/DevService.java
+++ src/com/huiguanjia/service/DevService.java
@@ -1,5 +1,22 @@
 package com.huiguanjia.service;
 
+import com.huiguanjia.dao.DevDao;
+
 public class DevService {
 
+	public int createIndustry(String industrycategory,String industryname,String industrycode)
+	{
+		DevDao aDevDao = new DevDao();
+		int res = aDevDao.addIndustry(industrycategory,industryname,industrycode);
+		
+		return res;
+	}
+	
+	public int newcity(String province,String cityname,String citycode)
+	{
+		DevDao aDevDao = new DevDao();
+		int res = aDevDao.addProvinceAndCity(province,cityname,citycode);
+		
+		return res;
+	}
 }
